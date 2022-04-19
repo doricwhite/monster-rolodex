3ba1b3167472dd86c296ccc2ab2ab99f047a5489
Doric <doricwhite@gmail.com>
2022-04-19 16:43:15 -0500

    Implement search field.

--- src/App.js
+++ src/App.js
@@ -9,10 +9,16 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: "",
     };
+
+    console.log("1. constructor run");
   }
 
+  /*Obtains information from URL (API request) */
   componentDidMount() {
+    console.log("3. componentDidMount run");
+
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((res) => res.json())
       .then((users) =>
@@ -21,16 +27,38 @@ class App extends Component {
             return { monsters: users };
           },
           () => {
-            console.log(this.state.monsters);
+            console.log(this.state);
           }
         )
       );
   }
 
   render() {
+    console.log("2: render run");
+
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
+        {/* Search Input */}
+        <input
+          className="search-box"
+          type="search"
+          placeholder="Search Monsters"
+          /* Action for when name is searched */
+          onChange={(evt) => {
+            const searchField = evt.target.value.toLocaleLowerCase();
+
+            this.setState(() => {
+              return { searchField };
+            });
+          }}
+        />
+
+        {/* Display Monster Information */}
+        {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
